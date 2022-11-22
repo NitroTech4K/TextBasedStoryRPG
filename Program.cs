b10b11da9761c8e3593f40f4d9d057ea3232d1c3
NitroTech 4K <crtraer@gmail.com>
2022-11-22 10:59:27 -0400

    Demo concept started

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace TextBasedStoryRPG
     {
         static void Main(string[] args)
         {
+
+            string words = "The dog is cute today and all days; pet dog; kick dog;2;3; you pet the dog but find The Cat, its Better then Dog; pet cat; don't kick cat you monster;4;5";
+
+
+            string[] pages = words.Split(';');
+
+            Console.WriteLine(pages[3]);
+
+
+            Console.ReadKey(true);
         }
     }
 }
