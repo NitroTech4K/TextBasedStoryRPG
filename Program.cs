2f239c6fe9ed4a94611bbd5a1f5ab51ec717f689
NitroTech 4K <crtraer@gmail.com>
2022-11-28 18:41:47 -0400

    Started input system

--- Program.cs
+++ Program.cs
@@ -8,13 +8,31 @@ namespace TextBasedStoryRPG
 {
     internal class Program
     {
+        static string[] Pages =
+            {
+            "The dog is cute today and all days; pet dog; kick dog;2;3; you pet the dog but find The Cat, its Better then Dog; pet cat; don't kick cat you monster;4;5",
+            "He's happy.; Give treat; Walk away;"
+            };
+        
+       
+        static int choices = 0;
+        
         static void Main(string[] args)
         {
+            while (true)
+            {
+            SplitStory();
+            
+            inputManager();
+   
+            Console.Clear();
+            }
+        }
 
-            string words = "The dog is cute today and all days; pet dog; kick dog;2;3; you pet the dog but find The Cat, its Better then Dog; pet cat; don't kick cat you monster;4;5";
+        static void SplitStory()
+        {
 
-
-            string[] pagesElements = words.Split(';');
+            string[] pagesElements = Pages[choices].Split(';');
 
             Console.WriteLine(pagesElements[0]);
             Console.WriteLine("");
@@ -22,9 +40,16 @@ namespace TextBasedStoryRPG
             Console.WriteLine("");
             Console.WriteLine(pagesElements[1]);
             Console.WriteLine(pagesElements[2]);
+        }
 
+        static void inputManager()
+        {
+            ConsoleKeyInfo input = Console.ReadKey(true);
 
-            Console.ReadKey(true);
+            if (input.Key == ConsoleKey.D1)
+            {
+                choices = 1;
+            }
         }
     }
 }
