5bd8d262a524b2d29a0c26dcd69fa985812cb1bf
NitroTech 4K <crtraer@gmail.com>
2022-11-28 11:30:23 -0400

    Show options and story, still need inputs

--- Program.cs
+++ Program.cs
@@ -14,9 +14,14 @@ namespace TextBasedStoryRPG
             string words = "The dog is cute today and all days; pet dog; kick dog;2;3; you pet the dog but find The Cat, its Better then Dog; pet cat; don't kick cat you monster;4;5";
 
 
-            string[] pages = words.Split(';');
+            string[] pagesElements = words.Split(';');
 
-            Console.WriteLine(pages[3]);
+            Console.WriteLine(pagesElements[0]);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(pagesElements[1]);
+            Console.WriteLine(pagesElements[2]);
 
 
             Console.ReadKey(true);
