0f13e7b9f8494f08035cf65bc85dd5134a13f300
NitroTech 4K <crtraer@gmail.com>
2022-11-29 18:13:07 -0400

    End pages bug (Needs fixing)

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace TextBasedStoryRPG
         static string[] Pages = //Page is in brackets below
             {                          //1 means go to page 1, 2 means go to page 2
             "The dog is cute today and all days; pet dog; kick dog; 1; 2", //page 0
-            " you pet the dog but find The Cat, its Better then Dog; pet cat; kick cat; 3; 4", //page 1
-            "You kicked the dog, it whimpers and runs away. You're alone.",  //page 2
+            "you pet the dog but find The Cat, its Better then Dog; pet cat; kick cat; 3; 4", //page 1
+            "You kicked the dog, it whimpers and runs away. You're alone.;end",  //page 2
             "You pet the cat, the dog whimpers and runs away. You abandoned him.",//Page 3
             "You kick the cat. It hisses and runs off. You're with the dog now.", //Page 4
             };
@@ -41,24 +41,37 @@ namespace TextBasedStoryRPG
         static void SplitStory() //Splits the story string up, separated by ;. Also a display methoud of options.
         {
             pagesElements = Pages[choices].Split(';');
-            for (int i = 0; i < pagesElements.Length - 4; i = i + 1) // continue to count until max amount of splits, minus 4, to not show the choices and next pages.
-            {
-                Console.WriteLine(pagesElements[i]); //First part of the story string of text
-            } 
 
 
-            if (pagesElements.Length > 1)
+            if (pagesElements) //If there's no splits
             {
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine(pagesElements[pagesElements.Length - 4]); //Option 1
-                Console.WriteLine(pagesElements[pagesElements.Length - 3]); //Option 2
+                Console.WriteLine("r");
             }
-            else 
+
+            else
             {
-                play = false;
+                for (int i = 0; i < pagesElements.Length - 4; i = i + 1) // continue to count until max amount of splits, minus 4, to not show the choices and next pages.
+                {
+                    Console.WriteLine(pagesElements[i]); //First part of the story string of text
+                } 
+
+
+                if (pagesElements.Length > 1)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine(pagesElements[pagesElements.Length - 4]); //Option 1
+                    Console.WriteLine(pagesElements[pagesElements.Length - 3]); //Option 2
+                }
+                else 
+                {
+                    play = false;
+                }
+
             }
+
+
 
         }
 
